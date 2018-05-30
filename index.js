2e682df095d8894e3904ca15ab3c6be427e9ce3c
Edward Mixon <edwardchase@gatech.edu>
2018-05-30 20:28:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var string = 'Hello'
+var string = 'HELLO'
 
 function shout(string) {
   return string.toUpperCase()
@@ -11,4 +11,12 @@ function whisper(string) {
 function logShout(string) {
   string = string.toUpperCase
 console.log(`${string}`)
+}
+
+
+function sayHiToGrandma(string) {
+string.toUpperCase === string
+return 'Yes Indeed!'
+string.toLowerCase === string
+return 'I cant hear you!'
 }
