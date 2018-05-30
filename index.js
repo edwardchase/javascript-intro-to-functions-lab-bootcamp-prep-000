92c1db6471b4cea3b90c9d920d27462bb9aeba0b
Edward Mixon <edwardchase@gatech.edu>
2018-05-30 20:57:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ return 'YES INDEED!'}
 else if (string.toLowerCase() === string) {
 return 'I can\'t hear you!'}
 else if (string === 'I love you, Grandma.') {
-  return 'I love you, too'}
+  return 'I love you, too.'}
 }
